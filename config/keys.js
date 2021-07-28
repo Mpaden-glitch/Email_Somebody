6491fc9432b8f913d42a4af26173b9323c0af51f
Mpaden-glitch <53572019+Mpaden-glitch@users.noreply.github.com>
2021-07-28 17:46:14 -0500

    finished product

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,8 @@
-//keys - figure out what set of keys to return
-
+// keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
-    //we are in prod
-    module.exports = require('./prod');
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
 } else {
-    //we are in dev
-    module.exports = require('./dev');
+  // we are in development - return the dev keys!!!
+  module.exports = require('./dev');
 }
--- services/passport.js
+++ services/passport.js
@@ -1,37 +1,37 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
 const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
-        done(null, user);
-    });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
-    new GoogleStrategy(
+  new GoogleStrategy(
     {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-    }, 
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
     async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id })
-        
-        if (existingUser) {
-            //we already have a record with given profile id
-           return done(null, existingUser);
-        }
-            //we don't have a user with that id make a new user record
-            const user = await new User({ googleId: profile.id }).save()
-            done(null, user);
-    })
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
 );
